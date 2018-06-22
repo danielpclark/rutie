cf36386a1c88d9ee1aebf603b1c2d26116fe687a
Daniel P. Clark <6ftdan@gmail.com>
2018-06-22 09:49:56 -0400

    Restore original build.rs options

--- build.rs
+++ build.rs
@@ -57,10 +57,43 @@ fn link() {
     let _ = copy(target_file(), destination);
 }
 
+fn use_libdir() {
+    println!("cargo:rustc-link-search={}", rbconfig("libdir"));
+}
+
+fn transform_lib_args(rbconfig_key: &str, replacement: &str) -> String {
+    rbconfig(rbconfig_key).replace("-l", replacement)
+}
+
+fn use_static() {
+    // Ruby gives back the libs in the form: `-lpthread -lgmp`
+    // Cargo wants them as: `-l pthread -l gmp`
+    println!("cargo:rustc-flags={}", transform_lib_args("LIBS", "-l "));
+}
+
+fn use_dylib() {
+    use_libdir();
+    println!("cargo:rustc-link-lib=dylib={}", rbconfig("RUBY_SO_NAME"));
+}
+
 fn main() {
-    if cfg!(feature="test") {
+		if cfg!(feature="test") {
         println!("cargo:rustc-link-search={}", rbconfig("libdir"));
         println!("cargo:rustc-link-lib=dylib={}", rbconfig("RUBY_SO_NAME"));
+    } else if rbconfig("target_os") != "mingw32" && env::var_os("RUBY_STATIC").is_some() {
+        use_static()
+    } else {
+        match rbconfig("ENABLE_SHARED").as_str() {
+            "no" => use_static(),
+            "yes" => use_dylib(),
+            _ => {
+                let msg = "Error! Couldn't find a valid value for \
+                RbConfig::CONFIG['ENABLE_SHARED']. \
+                This may mean that your ruby's build config is corrupted. \
+                Possible solution: build a new Ruby with the `--enable-shared` configure opt.";
+                panic!(msg)
+            }
+        }
     }
     link();
 }
