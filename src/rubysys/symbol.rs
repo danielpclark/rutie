ca939b1d711658b9bcfeaf23fee51bdef9735b08
Daniel P. Clark <6ftdan@gmail.com>
2018-07-07 10:55:57 -0400

    symbol signatures

--- src/rubysys/symbol.rs
+++ src/rubysys/symbol.rs
@@ -1,7 +1,13 @@
 use rubysys::types::{c_char, Id, Value};
 
 extern "C" {
+    // VALUE
+    // rb_id2sym(ID x)
     pub fn rb_id2sym(id: Id) -> Value;
+    // const char *
+    // rb_id2name(ID id)
     pub fn rb_id2name(id: Id) -> *const c_char;
+    // ID
+    // rb_sym2id(VALUE sym)
     pub fn rb_sym2id(id: Value) -> Id;
 }
