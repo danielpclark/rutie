7a7c8c94cda0e41eb500b607930413a07c941327
Daniel P. Clark <6ftdan@gmail.com>
2018-07-07 10:41:06 -0400

    float signatures

--- src/rubysys/float.rs
+++ src/rubysys/float.rs
@@ -1,6 +1,10 @@
 use rubysys::types::{c_double, Value};
 
 extern "C" {
+    // VALUE
+    // rb_float_new(double d)
     pub fn rb_float_new(num: f64) -> Value;
+    // double
+    // rb_num2dbl(VALUE val)
     pub fn rb_num2dbl(num: Value) -> c_double;
 }
