42f3b859edd73ffdeaff557995c1ce38414e187d
Daniel P. Clark <6ftdan@gmail.com>
2018-07-07 11:09:21 -0400

    type data signatures

--- src/rubysys/typed_data.rs
+++ src/rubysys/typed_data.rs
@@ -1,10 +1,17 @@
 use rubysys::types::{c_char, c_void, c_int, size_t, Value};
 
 extern "C" {
+    // void *
+    // rb_check_typeddata(VALUE obj, const rb_data_type_t *data_type)
     pub fn rb_check_typeddata(object: Value, data_type: *const RbDataType) -> *mut c_void;
+    // int
+    // rb_typeddata_inherited_p(const rb_data_type_t *child, const rb_data_type_t *parent)
     pub fn rb_typeddata_inherited_p(child: *const RbDataType, parent: *const RbDataType) -> c_int;
+    // int
+    // rb_typeddata_is_kind_of(VALUE obj, const rb_data_type_t *data_type)
     pub fn rb_typeddata_is_kind_of(object: Value, data_type: *const RbDataType) -> c_int;
-
+    // VALUE
+    // rb_data_typed_object_wrap(VALUE klass, void *datap, const rb_data_type_t *type)
     pub fn rb_data_typed_object_wrap(klass: Value,
                                      data: *mut c_void,
                                      data_type: *const RbDataType)
