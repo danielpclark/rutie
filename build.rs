3ec60b3a49e0e54a81f54a01072138ff6b2f5eec
Daniel P. Clark <6ftdan@gmail.com>
2018-07-25 21:38:29 -0400

    Add ability to disable pkg-config

--- build.rs
+++ build.rs
@@ -26,7 +26,7 @@ fn rbconfig(key: &str) -> String {
 }
 
 fn set_env_pkg_config() {
-    if let None = env::var_os("PKG_CONFIG_PATH") {
+    if env::var_os("PKG_CONFIG_PATH").is_none() {
         let key = "PKG_CONFIG_PATH";
         let value = Path::new(&rbconfig("libdir")).join("pkgconfig");
         std::env::set_var(key, &value);
@@ -54,28 +54,35 @@ fn use_static() {
     // Ruby gives back the libs in the form: `-lpthread -lgmp`
     // Cargo wants them as: `-l pthread -l gmp`
     println!("cargo:rustc-flags={}", transform_lib_args("LIBS", "-l "));
-    ci_stderr_log!("Using static linker flags{}", "");
+    ci_stderr_log!("Using static linker flags");
 }
 
 fn use_dylib() {
     use_libdir();
     println!("cargo:rustc-link-lib=dylib={}", rbconfig("RUBY_SO_NAME"));
-    ci_stderr_log!("Using dynamic linker flags{}", "");
+    ci_stderr_log!("Using dynamic linker flags");
 }
 
 fn main() {
     // Ruby programs calling Rust don't need cc linking
     if let None = std::env::var_os("NO_LINK_RUTIE") {
-        // Ruby includes pkgconfig under their lib dir
-        set_env_pkg_config();
 
-        match pkg_config::Config::new().atleast_version(trim_teeny(&ruby_version())).probe("ruby") {
-            Ok(_) => return,
-            Err(err) => ci_stderr_log!("{:?}", err),
+        if let None = env::var_os("RUTIE_NO_PKG_CONFIG") {
+            // Ruby often includes pkgconfig under their lib dir
+            set_env_pkg_config();
+
+            // To disable the use of pkg-config set the environment variable `RUTIE_NO_PKG_CONFIG`
+            match pkg_config::Config::new().atleast_version(trim_teeny(&ruby_version())).probe("ruby") {
+                Ok(_) => {
+                    ci_stderr_log!("pkg-config is being used");
+                    return;
+                },
+                Err(err) => ci_stderr_log!("{:?}", err),
+            }
         }
         
         if rbconfig("target_os") != "mingw32" && env::var_os("RUBY_STATIC").is_some() {
-            ci_stderr_log!("Not mingw && RUBY_STATIC exists{}", "");
+            ci_stderr_log!("Not mingw && RUBY_STATIC exists");
             use_static()
         } else {
             match rbconfig("ENABLE_SHARED").as_str() {
@@ -86,6 +93,7 @@ fn main() {
                     RbConfig::CONFIG['ENABLE_SHARED']. \
                     This may mean that your ruby's build config is corrupted. \
                     Possible solution: build a new Ruby with the `--enable-shared` configure opt.";
+                    ci_stderr_log!("{}", &msg);
                     panic!(msg)
                 }
             }
