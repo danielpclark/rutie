bba2823cb741220fe067a331f2314002f52b3cb7
irxground <irxnjhtchlnrw@gmail.com>
2019-05-08 22:17:23 +0900

    Clarify integer type name

--- src/binding/fixnum.rs
+++ src/binding/fixnum.rs
@@ -1,15 +1,26 @@
 use rubysys::fixnum;
+use types::Value;
 
-use types::{SignedValue, Value};
-
-pub fn int_to_num(num: i64) -> Value {
-    unsafe { fixnum::rb_int2inum(num as SignedValue) }
+pub fn i32_to_num(num: i32) -> Value {
+    unsafe { fixnum::rb_int2inum(num as isize) }
 }
 
-pub fn num_to_int(num: Value) -> i32 {
-    unsafe { fixnum::rb_num2int(num) }
+pub fn i64_to_num(num: i64) -> Value {
+    unsafe { fixnum::rb_ll2inum(num) }
 }
 
-pub fn num_to_long(num: Value) -> i64 {
-    unsafe { fixnum::rb_num2long(num) }
+pub fn u64_to_num(num: u64) -> Value {
+    unsafe { fixnum::rb_ull2inum(num) }
+}
+
+pub fn num_to_i32(num: Value) -> i32 {
+    unsafe { fixnum::rb_num2int(num) as i32 }
+}
+
+pub fn num_to_i64(num: Value) -> i64 {
+    unsafe { fixnum::rb_num2ll(num) }
+}
+
+pub fn num_to_u64(num: Value) -> u64 {
+    unsafe { fixnum::rb_num2ull(num) }
 }
--- src/class/integer.rs
+++ src/class/integer.rs
@@ -53,7 +53,29 @@ impl Integer {
     /// 1 == 1
     /// ```
     pub fn to_i64(&self) -> i64 {
-        fixnum::num_to_long(self.value())
+        fixnum::num_to_i64(self.value())
+    }
+
+    /// Retrieves an `u64` value from `Integer`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rutie::{Integer, VM};
+    /// # VM::init();
+    ///
+    /// let integer = Integer::new(1);
+    ///
+    /// assert_eq!(integer.to_u64(), 1);
+    /// ```
+    ///
+    /// Ruby:
+    ///
+    /// ```ruby
+    /// 1 == 1
+    /// ```
+    pub fn to_u64(&self) -> u64 {
+        fixnum::num_to_u64(self.value())
     }
 
     /// Retrieves an `i32` value from `Integer`.
@@ -75,7 +97,7 @@ impl Integer {
     /// 1 == 1
     /// ```
     pub fn to_i32(&self) -> i32 {
-        fixnum::num_to_int(self.value())
+        fixnum::num_to_i32(self.value())
     }
 }
 
@@ -87,19 +109,31 @@ impl From<Value> for Integer {
 
 impl From<i64> for Integer {
     fn from(num: i64) -> Self {
-        Integer { value: fixnum::int_to_num(num) }
+        Integer { value: fixnum::i64_to_num(num) }
     }
 }
 
 impl Into<i64> for Integer {
     fn into(self) -> i64 {
-        fixnum::num_to_long(self.value())
+        fixnum::num_to_i64(self.value())
+    }
+}
+
+impl From<u64> for Integer {
+    fn from(num: u64) -> Self {
+        Integer { value: fixnum::u64_to_num(num) }
+    }
+}
+
+impl Into<u64> for Integer {
+    fn into(self) -> u64 {
+        fixnum::num_to_u64(self.value())
     }
 }
 
 impl Into<i32> for Integer {
     fn into(self) -> i32 {
-        fixnum::num_to_int(self.value())
+        fixnum::num_to_i32(self.value())
     }
 }
 
