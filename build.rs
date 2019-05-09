9045636ab97ab52c89d4744bea9df3c4d64ec78a
Edmund Kump <ekump@vericred.com>
2019-05-09 16:29:10 -0400

    check if libruby symlink exists before attempting to create one in build script

--- build.rs
+++ build.rs
@@ -177,7 +177,12 @@ fn ruby_lib_link_name() -> String {
     let source = format!("{}/{}", rbconfig("libdir"), so_file);
     let target = format!("{}/{}", destination, so_file);
 
-    if !Path::new(&target).exists() {
+    let sym_link_exists = match fs::read_link(&target) {
+        Ok(_) => true,
+        _ => false
+    };
+
+    if ! sym_link_exists {
         let _ = symlink(source, target).expect("symlink fail");
     }
 
