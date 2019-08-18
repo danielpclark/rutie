4bf2c89aeb217d978ec1fb5f37322682ed1492ea
Brian Bugh <brainbag@gmail.com>
2019-08-18 10:21:08 -0500

    Correctly handle Float conversion of Ruby numerics
    This adds support casting `Fixnum`, `Bignum`, and `Rational` to a `Float`, which is supported automatically by the C API.
    
    Fixes #99

--- src/class/float.rs
+++ src/class/float.rs
@@ -84,7 +84,11 @@ impl Object for Float {
 
 impl VerifiedObject for Float {
     fn is_correct_type<T: Object>(object: &T) -> bool {
-        object.value().ty() == ValueType::Float
+        let ty = object.value().ty();
+        ty == ValueType::Float
+            || ty == ValueType::Fixnum
+            || ty == ValueType::Bignum
+            || ty == ValueType::Rational
     }
 
     fn error_message() -> &'static str {
@@ -95,5 +99,41 @@ impl VerifiedObject for Float {
 impl PartialEq for Float {
     fn eq(&self, other: &Self) -> bool {
         self.equals(other)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::super::types::Value;
+    use super::super::super::{AnyException, Float, Object, LOCK_FOR_TEST, VM};
+
+    #[test]
+    fn test_numeric_conversion() {
+        let _guard = LOCK_FOR_TEST.write().unwrap();
+        VM::init();
+
+        let fixnum = eval_to_float("-81927").unwrap();
+        assert_eq!(fixnum.to_f64(), -81927.0);
+
+        let bignum = eval_to_float("2 ** 62").unwrap();
+        assert_eq!(bignum.to_f64(), 4611686018427387904.0);
+
+        let float = eval_to_float("123456789.0").unwrap();
+        assert_eq!(float.to_f64(), 123456789.0);
+
+        let rational = eval_to_float("1/2r").unwrap();
+        assert_eq!(rational.to_f64(), 0.5);
+
+        let rational_repeating = eval_to_float("-1/3r").unwrap();
+        assert_eq!(rational_repeating.to_f64(), -0.3333333333333333);
+
+        let string = eval_to_float("'5.0'");
+        assert!(string.is_err());
+
+        VM::exit(0);
+    }
+
+    fn eval_to_float(code: &str) -> Result<Float, AnyException> {
+        VM::eval(code).and_then(|x| x.try_convert_to::<Float>())
     }
 }
