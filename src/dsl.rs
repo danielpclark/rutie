d0ced8fb72566c0003ec61ddbd9acc6e101216d1
Andrew T. Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-11-16 21:52:01 -0600

    Support trailing commas in unsafe_methods macro
    Adds support for trailing commas in parameters lists of methods declared
    with the unsafe_methods macro.

--- src/dsl.rs
+++ src/dsl.rs
@@ -234,7 +234,7 @@ macro_rules! unsafe_methods {
         $rtself_name: ident,
         $(
             fn $method_name: ident
-            ($($arg_name: ident: $arg_type: ty),*) -> $return_type: ty $body: block
+            ($($arg_name: ident: $arg_type: ty),* $(,)?) -> $return_type: ty $body: block
             $(,)?
         )*
     ) => {
