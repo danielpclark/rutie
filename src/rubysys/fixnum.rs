6ad69be7c685c0c62262ede13f87e456d8011723
Jose Narvaez <jose.narvaez@shopify.com>
2023-02-22 17:59:01 +0000

    Fixnum neded to be converted back to extern declarations.

--- src/rubysys/fixnum.rs
+++ src/rubysys/fixnum.rs
@@ -1,40 +1,42 @@
 use super::types::Value;
 use libc::{c_long, c_longlong, c_short, c_ulong, c_ulonglong, c_ushort, intptr_t, uintptr_t};
-pub use rb_sys::{
+
+extern "C" {
     // VALUE
     // rb_int2inum(intptr_t n)
-    rb_int2inum,
-    // VALUE
-    // rb_ll2inum(LONG_LONG n)
-    rb_ll2inum,
-    // long
-    // rb_num2int(VALUE val)
-    rb_num2int,
-    // LONG_LONG
-    // rb_num2ll(VALUE val)
-    rb_num2ll,
-    // long
-    // rb_num2long(VALUE val)
-    rb_num2long,
-    // short
-    // rb_num2short(VALUE val)
-    rb_num2short,
-    // unsigned long
-    // rb_num2uint(VALUE val)
-    rb_num2uint,
-    // unsigned LONG_LONG
-    // rb_num2ull(VALUE val)
-    rb_num2ull,
-    // unsigned long
-    // rb_num2ulong(VALUE val)
-    rb_num2ulong,
-    // unsigned short
-    // rb_num2ushort(VALUE val)
-    rb_num2ushort,
+    pub fn rb_int2inum(num: libc::intptr_t) -> Value;
     // VALUE
     // rb_uint2inum(uintptr_t n)
-    rb_uint2inum,
+    pub fn rb_uint2inum(num: libc::uintptr_t) -> Value;
+    // VALUE
+    // rb_ll2inum(LONG_LONG n)
+    pub fn rb_ll2inum(num: libc::c_longlong) -> Value;
     // VALUE
     // rb_ull2inum(unsigned LONG_LONG n)
-    rb_ull2inum,
-};
+    pub fn rb_ull2inum(num: libc::c_ulonglong) -> Value;
+
+    // short
+    // rb_num2short(VALUE val)
+    pub fn rb_num2short(num: Value) -> libc::c_short;
+    // unsigned short
+    // rb_num2ushort(VALUE val)
+    pub fn rb_num2ushort(num: Value) -> libc::c_ushort;
+    // long
+    // rb_num2int(VALUE val)
+    pub fn rb_num2int(num: Value) -> libc::c_long;
+    // unsigned long
+    // rb_num2uint(VALUE val)
+    pub fn rb_num2uint(num: Value) -> libc::c_ulong;
+    // long
+    // rb_num2long(VALUE val)
+    pub fn rb_num2long(num: Value) -> libc::c_long;
+    // unsigned long
+    // rb_num2ulong(VALUE val)
+    pub fn rb_num2ulong(num: Value) -> libc::c_ulong;
+    // LONG_LONG
+    // rb_num2ll(VALUE val)
+    pub fn rb_num2ll(num: Value) -> libc::c_longlong;
+    // unsigned LONG_LONG
+    // rb_num2ull(VALUE val)
+    pub fn rb_num2ull(num: Value) -> libc::c_ulonglong;
+}
