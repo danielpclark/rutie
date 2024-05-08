d8fd986c00214bbe48babe5ca811f9b87896e164
Mike Yang <yfractal@gmail.com>
2024-05-08 23:08:57 +0800

    feat: added test

--- src/class/marshal.rs
+++ src/class/marshal.rs
@@ -1,6 +1,7 @@
 use crate::{
     binding::marshal,
     types::Value,
+    AnyObject, NilClass,
 };
 
 /// `Marshal`
@@ -11,11 +12,31 @@ pub struct Marshal {
 }
 
 impl Marshal {
-    pub fn load(port: Value) -> Value {
-        marshal::marshal_load(port)
+    /// Dump a Ruby object and load back
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rutie::{Boolean, VM, NilClass, Marshal};
+    /// # VM::init();
+    ///
+    /// let dumped = Marshal::dump(Boolean::new(true).into(), NilClass::new().into());
+    ///
+    /// assert_eq!(Marshal::load(dumped), Boolean::new(true).into());
+    /// ```
+    ///
+    /// Ruby:
+    ///
+    /// ```ruby
+    /// dumped =  Marshal::dump(true)
+    ///
+    /// Marshal::load(dumped) == true
+    /// ```
+    pub fn load(port: AnyObject) -> AnyObject {
+        marshal::marshal_load(port.into()).into()
     }
 
-    pub fn dump(val: Value, port: Value) -> Value {
-        marshal::marshal_dump(val, port)
+    pub fn dump(val: AnyObject, port: AnyObject) -> AnyObject {
+        marshal::marshal_dump(val.into(), port.into()).into()
     }
 }
